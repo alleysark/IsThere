e73bbac180a21bbfe030e851689186055833d643
sark <alleysark@gmail.com>
2012-11-21 04:11:54 +0900

    solve autowired problem

--- src/main/java/org/isthere/controller/DetailController.java
+++ src/main/java/org/isthere/controller/DetailController.java
@@ -1,36 +1,40 @@
 package org.isthere.controller;
 
-import net.sf.json.JSONObject;
+import org.isthere.connector.kipris.DetailPatentInfoGetter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import sock.InfoGetter;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Description.
- *
- * @author Edward KIM
- * @since 1.0
- */
 @Controller
 @RequestMapping("rest")
 public class DetailController {
 
-    @RequestMapping(value = "socket", method = RequestMethod.POST)
+    @Autowired
+    private DetailPatentInfoGetter detailPatentInfoGetter;
+
+    @RequestMapping(value = "detailPatentGetter", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public ResponseEntity<Map<String, Object>> socket(@RequestParam String reqAppNo) throws IOException {
+    public ResponseEntity<Map<String, Object>> detailPatentGetter(@RequestParam String reqAppNo) throws IOException {
         Map<String, Object> params = new HashMap<String, Object>();
+
         try {
-            InfoGetter infoGetter = new InfoGetter();
-            infoGetter.requestDetail(reqAppNo);
-            JSONObject obj = infoGetter.parseDataFromHtmlString();
-            params.put("result", obj);
+            Map<String,String > detailData =
+                detailPatentInfoGetter.requestDetail(reqAppNo);
+
+            if( null == detailData){
+                params.put("success", false);
+            }else{
+                params.put("success", true);
+                params.put("total", 1);
+                params.put("detInfo", detailData);
+            }
             return new ResponseEntity(params, HttpStatus.OK);
         } catch (Exception ex) {
             ex.printStackTrace();
