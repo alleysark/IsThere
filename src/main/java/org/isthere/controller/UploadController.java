af7abb984d743755a4217bb1efb85cbedee130a9
sark <alleysark@gmail.com>
2012-11-21 04:12:36 +0900

    solve upload mapping problem

--- src/main/java/org/isthere/controller/UploadController.java
+++ src/main/java/org/isthere/controller/UploadController.java
@@ -1,10 +1,63 @@
 package org.isthere.controller;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Description.
  *
  * @author Edward KIM
  * @since 1.0
  */
+@Controller
+@RequestMapping("rest")
 public class UploadController {
+
+    @RequestMapping(value = "upload", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> upload(HttpServletRequest req) throws IOException {
+        Map<String, Object> params = new HashMap<String, Object>();
+        if (!(req instanceof DefaultMultipartHttpServletRequest)) {
+            params.put("success", Boolean.FALSE);
+            return new ResponseEntity(params, HttpStatus.BAD_REQUEST);
+        }
+
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            DefaultMultipartHttpServletRequest request = (DefaultMultipartHttpServletRequest) req;
+            MultipartFile uploadedFile = request.getFile("uploadPath");
+            String originalFilename = uploadedFile.getOriginalFilename();
+
+            // TODO pass bytes array to image handler
+
+            System.out.println(originalFilename);
+            params.put("success", Boolean.TRUE);
+            return new ResponseEntity(params, HttpStatus.OK);
+        } catch (Exception ex) {
+            System.out.println("\t>> " + ex.getMessage());
+            params.put("success", Boolean.FALSE);
+            params.put("error", ex.getMessage());
+            return new ResponseEntity(params, HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+            IOUtils.closeQuietly(outputStream);
+        }
+    }
 }
