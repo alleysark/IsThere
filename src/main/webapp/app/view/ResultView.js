12951dcfe8b4177d92bdd13656d0abd61dba637b
sark <alleysark@gmail.com>
2012-11-21 10:00:20 +0900

    [un tested] construct a ResultView panel

--- src/main/webapp/app/view/ResultView.js
+++ src/main/webapp/app/view/ResultView.js
@@ -1,7 +1,54 @@
-/**
- * Created with IntelliJ IDEA.
- * User: AlleySark
- * Date: 12. 11. 21
- * Time: 오전 9:47
- * To change this template use File | Settings | File Templates.
- */
+Ext.define('IsThere.view.ResultView', {
+    extend:'Ext.view.View',
+    alias:'widget.resultView',
+
+    requires:[
+        'IsThere.view.CartList',
+        'Ext.grid.plugin.DragDrop'
+    ],
+
+    store:'ResultStore',
+
+    tpl:[
+        '<tpl for=".">',
+        '<div class="thumb-wrap" id="{appNo:stripTags}">',
+        '<div class="thumb"><img src="{url}" title="{appNo:htmlEncode}"></div>',
+        '<span class="app-no">{appNo:htmlEncode}</span>',
+        '</div>',
+        '</tpl>',
+        '<div class="x-clear"></div>'
+    ],
+    multiSelect:true,
+    height:310,
+    //trackOver:true,
+    //overItemCls:'x-item-over',
+    itemSelector:'div.thumb-wrap',
+    emptyText:'No images to display',
+    plugins:[
+        Ext.create('Ext.ux.DataView.DragSelector', {})
+    ],
+    listeners:{
+        selectionchange:function (dv, nodes) {
+            console.log('sel changed');
+        }
+    },
+    /*
+     viewConfig:{
+     plugins:{
+     ptype:'gridviewdragdrop',
+     ddGroup:'result-ddgroup',
+
+     enableDrag:true,
+     enableDrop:false
+     },
+     copy:true
+     },
+     */
+
+    initComponent:function () {
+        var me = this;
+
+        me.callParent(arguments);
+    }
+
+});
